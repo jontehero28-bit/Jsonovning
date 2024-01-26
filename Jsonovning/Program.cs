8d271c2613744cee19a7078f4627bc41bbb8c1da
Yuriy Petrov <Jurijp2005@gmail.com>
2024-01-26 10:24:24 +0100

    commit1

---
+++ Jsonovning/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+
+RestClient client = new RestClient("https://swapi.py4e.com/api/");
+Console.WriteLine("Which Star Wars Character do you want to see? Write Only Number.");
+
+String starWarsName = Console.ReadLine();
+RestRequest request = new($"people/{starWarsName}/");
+
+RestResponse responce = client.GetAsync(request).Result;
+
+Starwars p = JsonSerializer.Deserialize<Starwars>(responce.Content);
+
+Console.WriteLine(p.name);
+
+
+//Console.WriteLine(responce.Content);
+//File.WriteAllText("starwars.json", responce.Content);
+
+Console.ReadLine();
---
+++ Jsonovning/StarWars.cs
@@ -0,0 +1,7 @@
+
+public class Starwars
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string mass { get; set; }
+}
